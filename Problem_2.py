0404dd6813100f4620932c4a8e6b899ae59842ed
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-08-25 21:31:27 +0530

    Add files via upload

---
+++ Problem_2.py
@@ -0,0 +1,13 @@
+def Fibonacci():
+    a=1
+    b=2
+    ans=0
+    while(a<=4*10**6):
+        if a%2==0:
+            ans+=a
+        c=a+b
+        a=b
+        b=c
+        
+    return ans
+print(Fibonacci())
