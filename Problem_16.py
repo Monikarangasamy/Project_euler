f0456ce707637b13cd8706c7610ce7e246e38c5c
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-08-25 23:37:27 +0530

    Add files via upload

---
+++ Problem_16.py
@@ -0,0 +1,15 @@
+#problem number 16
+#power digit sum
+
+def power_sum():
+    n=2**1000
+    result=""
+    sum=0
+    while(n!=0):
+        digit=n%10
+        sum+=digit
+        n=n//10
+        
+    return sum
+print(power_sum())
+
