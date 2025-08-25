e92b6b8142dbae44156168f5ed78766d099a9d15
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-08-25 23:24:20 +0530

    Add files via upload

---
+++ Problem_4.py
@@ -0,0 +1,20 @@
+#problem number 4
+#Largest palindrome product number
+
+def palindrome():
+    max_palindrome = 0
+    for i in range(999, 100, -1):
+        for j in range(999, 100, -1):
+            product = i * j
+            temp = product
+            result = ""
+            while temp != 0: #reverse a interger
+                digit = temp % 10
+                temp = temp // 10
+                result += str(digit)
+            if str(product) == result: # check palindrome
+                if product > max_palindrome:
+                    max_palindrome = product
+    return max_palindrome
+
+print(palindrome())
