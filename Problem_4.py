40bb90b513bf94853a9972652d717fe232ba7e88
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-08-26 09:01:25 +0530

    Update Problem_4.py

--- Problem_4.py
+++ Problem_4.py
@@ -18,3 +18,22 @@ def palindrome():
     return max_palindrome
 
 print(palindrome())
+
+#method 2 
+
+def ispalindrome(n):
+    original = str(n)
+    reverse = original[::-1]
+    return original == reverse
+
+def palindrome():
+    max_palindrome = 0
+    for i in range(999, 100, -1):
+        for j in range(999, 100, -1):
+            product = i * j
+            if product > max_palindrome and ispalindrome(product):
+                    max_palindrome = product
+    return max_palindrome
+
+print(palindrome())
+
