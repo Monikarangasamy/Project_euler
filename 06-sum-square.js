e1f3a37b325bd1cd22691fc60e5637d75af14992
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-08-26 21:19:01 +0530

    Delete 06-sum-square.js

--- 06-sum-square.js
+++
@@ -1,8 +0,0 @@
-function sumSquareDifference() {
-    const n = 100;
-    const squareOfSum = ((n * (n + 1)) / 2) ** 2;
-    const sumOfSquare = (n * (n + 1) * (2 * n + 1)) / 6;
-    return squareOfSum - sumOfSquare;
-}
-
-console.log(sumSquareDifference());
