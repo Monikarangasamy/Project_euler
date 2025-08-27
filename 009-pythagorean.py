2ab5d99555f76ccc69dc7eedbd433237ef1c53f0
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-08-27 16:37:56 +0530

    Add files via upload

---
+++ 009-pythagorean.py
@@ -0,0 +1,19 @@
+# def pythagorean_triplet():
+#     for a in range(1, 1000):
+#         for b in range(a + 1, 1000):
+#             c = 1000 - a - b
+#             if a * a + b * b == c * c and a + b + c == 1000:
+#                 return a * b * c
+            
+# print(pythagorean_triplet())
+
+
+
+def pythagorean_triplet():
+    for a in range(1, 1000):
+            b =  ( 500000 - 1000 * a ) // (1000 - a)  
+            c = 1000 - a - b
+            if a * a + b * b == c * c:
+                return a * b * c
+
+print(pythagorean_triplet())
