c26284c4ea48b81dcf1f49c689d9cdf46d6d7a29
Monika Rangasamy <146821389+Monikarangasamy@users.noreply.github.com>
2025-08-27 16:37:27 +0530

    Delete 009-pythagorean.py

--- 009-pythagorean.py
+++
@@ -1,9 +0,0 @@
-def pythagorean_triplet():
-    for a in range(1, 1000):
-        for b in range(a + 1, 1000):
-            c = 1000 - a - b
-            if a * a + b * b == c * c and a + b + c == 1000:
-                return a * b * c
-            
-
-print(pythagorean_triplet())
